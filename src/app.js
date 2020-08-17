3da48959667110d6ec2ecf4c0b18f37101fcae60
Lucas <lucascarmo6@gmail.com>
2020-08-17 17:35:05 -0300

    added repositories crud methods

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,57 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+  const updatedRepository = {
+    title,
+    url,
+    techs,
+    id,
+    likes: repositories[repoIndex].likes,
+  };
+  repositories[repoIndex] = updatedRepository;
+  return response.status(200).json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+  repositories[repoIndex].likes++;
+  return response.status(200).json(repositories[repoIndex]);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("API running on port: 3333");
+});
